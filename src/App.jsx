e4013b971b2047988108205b2d6d94e1569b8205
Victoria Ehrlich-Adam <victoriaea@Victorias-iMac.local>
2025-10-31 19:50:21 +0000

    Fix component name casing (YoY)

--- src/App.jsx
+++ src/App.jsx
@@ -118,7 +118,7 @@ export default function App() {
           </div>
 
           <div style={{ maxWidth: CHART_MAX, margin: "12px auto 0" }}>
-            <InflationYoyTwoSeries />
+            <InflationYoYTwoSeries />
           </div>
         </section>
 
